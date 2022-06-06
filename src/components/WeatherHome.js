7755e2141d15af23f627bc0be78ae60b31d0a686
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-06-06 16:06:48 +0200

    added weather

--- src/components/WeatherHome.js
+++ src/components/WeatherHome.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react';
 
-const WeatherHome = () => {
-  return <section>The Weather component</section>;
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
+
+const Weather = () => {
+	const { data, isLoading, errorMessage } = useOpenWeather({
+		key: 'a97e7de7f03fde16353def46be2c83e7',
+		lat: '57.64188017795392',
+		lon: '18.292565198468033',
+		lang: 'en',
+		unit: 'metric', // values are (metric, standard, imperial)
+	});
+	return (
+		<ReactWeather
+			isLoading={isLoading}
+			errorMessage={errorMessage}
+			data={data}
+			lang='en'
+			locationLabel='Visby'
+			unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+			// showForecast
+		/>
+	);
 };
 
-export default WeatherHome;
+export default Weather;
