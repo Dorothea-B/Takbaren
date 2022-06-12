604087d98c0e92af66aa807a0f9b96e5502960fc
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-06-12 20:31:17 +0200

    moremap

--- src/components/HeaderHome.js
+++ src/components/HeaderHome.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components/macro";
-import Logo from "../assets/Takbar_logo.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components/macro';
+import Logo from '../assets/Takbar_logo.png';
 
 const HeaderHome = () => {
-  return (
-    <>
-      <section>
-        <Link className="logo" to="/">
-          <Img src={Logo} />
-        </Link>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section>
+				<Link className='logo' to='/'>
+					<Img src={Logo} />
+				</Link>
+			</section>
+		</>
+	);
 };
 
 export default HeaderHome;
 
 const Img = styled.img`
-  max-width: 50%;
-  display: flex;
-  justify-content: center;
-  margin: 1rem;
+	max-width: 50%;
+	display: flex;
+	justify-content: center;
+	margin: 1rem;
 `;
--- src/components/MapHome.js
+++ src/components/MapHome.js
@@ -1,10 +1,29 @@
-import React from 'react';
+// import React, { useRef, useEffect, useState } from 'react';
+// import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+// import 'mapbox-gl/dist/mapbox-gl.css';
 
-const MapHome = () => {
-	return <section>
-		Lorem</section>;
-		
-};
+// const MapHome = () => {
+// 	mapboxgl.accessToken =
+// 		'pk.eyJ1IjoibWFyaWFubmVhcmRpbiIsImEiOiJjbDQ3Mmc3ZTkwM3drM2tsOXh5NmtqOWhjIn0.yZxjb02HmWVm6P7rG8i8fA';
+// 	const mapContainer = useRef(null);
+// 	const map = useRef(null);
+// 	const [lng] = useState(18.292565198468033);
+// 	const [lat] = useState(57.64188017795392);
+// 	const [zoom] = useState(17);
+// 	useEffect(() => {
+// 		if (map.current) return; // initialize map only once
+// 		map.current = new mapboxgl.Map({
+// 			container: mapContainer.current,
+// 			style: 'mapbox://styles/mapbox/streets-v11',
+// 			center: [lng, lat],
+// 			zoom: zoom,
+// 		});
 
-
-export default MapHome;
+// 		return (
+// 			<div>
+// 				<div ref={mapContainer} className='map-container' />
+// 			</div>
+// 		);
+// 	});
+// };
+// export default MapHome;
