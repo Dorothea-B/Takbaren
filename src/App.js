164c5f0e4f94b71b9b71815e812cb33c17158cf3
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-06-12 20:32:25 +0200

    merge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 // import WeatherApp from "./components/WeatherApp";
 
 // import MadLibList from './Pages/MadLibList';
@@ -16,25 +15,8 @@ import NotFound from './Pages/NotFound';
 // import styles from "./App.module.css";
 
 import './App.css';
-mapboxgl.accessToken =
-	'pk.eyJ1IjoibWFyaWFubmVhcmRpbiIsImEiOiJjbDQ3Mmc3ZTkwM3drM2tsOXh5NmtqOWhjIn0.yZxjb02HmWVm6P7rG8i8fA';
 
 const App = () => {
-	const mapContainer = useRef(null);
-	const map = useRef(null);
-	const [lng] = useState(18.292565198468033);
-	const [lat] = useState(57.64188017795392);
-	const [zoom] = useState(17);
-	useEffect(() => {
-		if (map.current) return; // initialize map only once
-		map.current = new mapboxgl.Map({
-			container: mapContainer.current,
-			style: 'mapbox://styles/mapbox/streets-v11',
-			center: [lng, lat],
-			zoom: zoom,
-		});
-	});
-
 	return (
 		<div className='App'>
 			<header>
@@ -49,14 +31,9 @@ const App = () => {
 				<Route path='/openinghours' element={<OpenHours />} />
 				<Route path='*' element={<NotFound />} />
 			</Routes>
-			<div />
-			<div>
-				<div ref={mapContainer} className='map-container' />
-			</div>
 		</div>
 	);
 };
 
 export default App;
-
 /* <Route path="/mad-libs/:slug" element={<MadLib />} /> */
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,66 +1,66 @@
-import React from "react";
-import styled from "styled-components/macro";
+import React from 'react';
+import styled from 'styled-components/macro';
 
 // import NotFound from "./NotFound";
-import "../index.css";
+import '../index.css';
 
-import ContactHome from "../components/ContactHome";
-import IntroTextHome from "../components/IntroTextHome";
-import MenuHome from "../components/MenuHome";
-import OpenHoursHome from "../components/OpenHoursHome";
-import SocialMediaHome from "../components/SocialMediaHome";
-import WeatherApp from "../components/WeatherApp";
-import ScrollUpBtn from "../components/ScrollUpBtn";
-import HeaderImg from "../assets/Header-img.jpg";
+import ContactHome from '../components/ContactHome';
+import IntroTextHome from '../components/IntroTextHome';
+import MenuHome from '../components/MenuHome';
+import OpenHoursHome from '../components/OpenHoursHome';
+import SocialMediaHome from '../components/SocialMediaHome';
+import WeatherApp from '../components/WeatherApp';
+import ScrollUpBtn from '../components/ScrollUpBtn';
+import HeaderImg from '../assets/Header-img.jpg';
 // import MapHome from '../components/MapHome';
 
 const HomePageWrapper = styled.div`
-  width: calc(100% - 30px);
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+	width: calc(100% - 30px);
+	margin: auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 
-  & > * {
-    margin-top: 2rem;
-  }
+	& > * {
+		margin-top: 2rem;
+	}
 
-  @media (max-width: 768px) {
-  }
+	@media (max-width: 768px) {
+	}
 `;
 
 const CardLight = styled.div`
-  background-color: var(--clr-white);
-  padding: 2rem;
-  width: 100%;
+	background-color: var(--clr-white);
+	padding: 2rem;
+	width: 100%;
 `;
 
 const Home = () => {
-  return (
-    <section>
-      <img src={HeaderImg} alt="header" />
-      <h1>how fast can you see this?</h1>
-      <HomePageWrapper>
-        <CardLight>
-          <IntroTextHome />
-        </CardLight>
-        <MenuHome />
-        <CardLight>
-          <OpenHoursHome />
-        </CardLight>
+	return (
+		<section>
+			<img src={HeaderImg} alt='header' />
+			<h1>how fast can you see this?</h1>
+			<HomePageWrapper>
+				<CardLight>
+					<IntroTextHome />
+				</CardLight>
+				<MenuHome />
+				<CardLight>
+					<OpenHoursHome />
+				</CardLight>
 
-        <WeatherApp />
-        <SocialMediaHome />
+				<WeatherApp />
+				<SocialMediaHome />
+				{/* <MapHome /> */}
+				<CardLight>
+					<ContactHome />
+				</CardLight>
+			</HomePageWrapper>
 
-        <CardLight>
-          <ContactHome />
-        </CardLight>
-      </HomePageWrapper>
-
-      <ScrollUpBtn />
-    </section>
-  );
+			<ScrollUpBtn />
+		</section>
+	);
 };
 
 export default Home;
