dc16d1d1a5baacd86e6b4b098893fb171dd76e6b
Dorothea-B <barre.dorothea@gmail.com>
2022-06-15 13:03:45 +0200

    static coordinates for map

--- src/components/MapHome.js
+++ src/components/MapHome.js
@@ -11,15 +11,17 @@ const MapHome = () => {
 	
 	const mapContainer = useRef(null);
 	const map = useRef(null);
-	const [lng, setLng] = useState(18.292565198468033);
-	const [lat, setLat] = useState(57.64188017795392);
+	// const [lng, setLng] = useState(18.292565198468033);
+	// const [lat, setLat] = useState(57.64188017795392);
 	const [zoom, setZoom] = useState(17);
+
+
 	useEffect(() => {
-		// if (map.current) return; // initialize map only once
+		if (map.current) return; // initialize map only once
 		map.current = new mapboxgl.Map({
 			container: mapContainer.current,
 			style: 'mapbox://styles/dorothea-b/cl4ea652m000114mjgcng4s0t',
-			center: [lng, lat],
+			center: [18.292565198468033, 57.64188017795392],
 			zoom: zoom,
 		});
 	});
