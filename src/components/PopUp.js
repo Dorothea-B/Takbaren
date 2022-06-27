982b668882a7fee6b0fc5f09975200ca9d635bc6
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-06-27 18:45:45 +0200

    changed color of cookie popup

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,39 +1,38 @@
-import React from "react";
-import CookieConsent from "react-cookie-consent";
-import "../index.css";
-import styled from "styled-components/macro";
+import React from 'react';
+import CookieConsent from 'react-cookie-consent';
+import '../index.css';
+import styled from 'styled-components/macro';
 
 const PopUp = () => {
-  return (
-    <div>
-      <CookieConsent
-        location='bottom'
-        buttonText='Accept'
-        cookieName='myAwesomeCookieName2'
-        style={{ background: "#F6BDAC", color: "#343B3F" }}
-        buttonStyle={{
-          background: "#E5E55E5",
-          color: "#6F1132",
-          fontSize: "13px",
-          fontFamily: "Poppins",
-        }}
-        expires={150}
-      >
-        <ErrorText>
-          This website uses cookies to enhance the user experience.
-        </ErrorText>
-      </CookieConsent>
-    </div>
-  );
+	return (
+		<div>
+			<CookieConsent
+				location='bottom'
+				buttonText='Accept'
+				cookieName='myAwesomeCookieName2'
+				style={{ background: '#6F1132', color: '#F7F6F0' }}
+				buttonStyle={{
+					background: '#E5E55E5',
+					color: '#6F1132',
+					fontSize: '13px',
+					fontFamily: 'Poppins',
+				}}
+				expires={150}>
+				<ErrorText>
+					This website uses cookies to enhance the user experience.
+				</ErrorText>
+			</CookieConsent>
+		</div>
+	);
 };
 
 export default PopUp;
 
 export const ErrorText = styled.p`
-  font-family: "Poppins", sans-serif;
-  font-weight: bold;
-  font-size: 16px;
-  font-weight: 500;
-  color: var(--clr-grey);
-  text-align: center;
+	font-family: 'Poppins', sans-serif;
+	font-weight: bold;
+	font-size: 16px;
+	font-weight: 500;
+	color: #f7f6f0;
+	text-align: center;
 `;
