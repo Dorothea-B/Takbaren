b4735baa5e073f901d0e95181461efb09db836cf
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-07-04 13:43:43 +0200

    did some changes to the popup cookie bar

--- src/toolcomponents/PopUp.js
+++ src/toolcomponents/PopUp.js
@@ -1,37 +1,65 @@
-import React from 'react';
-import CookieConsent from 'react-cookie-consent';
-import '../index.css';
-import styled from 'styled-components/macro';
+import React from "react";
+import CookieConsent from "react-cookie-consent";
+import "../index.css";
+import styled from "styled-components/macro";
 
 const PopUp = () => {
-	return (
-		<div>
-			<CookieConsent
-				location='bottom'
-				buttonText='Accept'
-				cookieName='myAwesomeCookieName2'
-				style={{ background: '#6F1132', color: '#F7F6F0' }}
-				buttonStyle={{
-					background: '#E5E55E5',
-					color: '#6F1132',
-					fontSize: '13px',
-					fontFamily: 'Poppins',
-				}}
-				expires={150}>
-				<ErrorText>
-					This website uses cookies to enhance the user experience.
-				</ErrorText>
-			</CookieConsent>
-		</div>
-	);
+  return (
+    <CookieConsent
+      location='bottom'
+      buttonText='I understand'
+      cookieName='CookiePopUp'
+      overlay='true'
+      style={{
+        background: "#F7F6F0",
+        boxShadow: "7px 7px 15px 1px rgba(52, 59, 63, 0.72)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "200px",
+        width: "300px",
+      }}
+      buttonStyle={{
+        background: "#6F1132",
+        color: "#F7F6F0",
+        fontSize: "14px",
+        fontFamily: "Poppins, sans-serif",
+        fontWeight: "bold",
+        cursor: "pointer",
+        border: "none",
+        letterSpacing: "0.25em",
+        textTransform: "uppercase",
+        padding: "0.5rem",
+        position: "absolute",
+        bottom: "0",
+        left: "0",
+        right: "0",
+        height: "3rem",
+        marginBottom: "2rem",
+      }}
+      expires={365}
+    >
+      <PopUpMessage>
+        This website uses cookies to enhance the user experience.
+      </PopUpMessage>
+    </CookieConsent>
+  );
 };
 
 export default PopUp;
 
-export const ErrorText = styled.p`
-	font-family: 'Poppins', sans-serif;
-	font-weight: bold;
-	font-size: 16px;
-	color: #f7f6f0;
-	text-align: center;
+export const PopUpMessage = styled.p`
+  font-family: "Poppins", sans-serif;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 16px;
+  color: var(--clr-dark);
+  text-align: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin-top: 2rem;
 `;
